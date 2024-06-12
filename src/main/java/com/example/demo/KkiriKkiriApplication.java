60f000285a14f7617930324743c1e7c530e1234f
Juhee <hongjh2004@gmail.com>
2024-06-12 13:37:51 +0900

    first commit

---
+++ src/main/java/com/example/demo/KkiriKkiriApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+@ComponentScan(basePackages = "dao")
+@ComponentScan(basePackages = "dto")
+@ComponentScan(basePackages = "controller")
+@ComponentScan(basePackages = "service")
+public class KkiriKkiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkiriKkiriApplication.class, args);
+		
+		System.out.println("끼리끼리 Application class");
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+<script src="/jquery-3.7.1.min.js"></script>
+<script>
+	$(document).ready(function() {
+		
+	}); // ready end
+</script>
+</head>
+<body>
+	<h1>KKIRIKKIRI</h1>
+	<h1>${ model }</h1>
+</body>
+</html>
