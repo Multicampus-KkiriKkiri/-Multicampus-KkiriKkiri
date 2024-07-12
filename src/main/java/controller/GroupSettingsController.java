ae847699ef7d45efa7ceb7c857407bf9abc87cd1
ByeongChanAn <byeongchan1120@gmail.com>
2024-07-12 19:31:56 +0900

    .

--- src/main/java/controller/GroupSettingsController.java
+++ src/main/java/controller/GroupSettingsController.java
@@ -8,33 +8,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/groupsettings")
 public class GroupSettingsController {
-	/*
-	 @Autowired
-	 private GroupService groupService;
-
-	    @GetMapping("/manage")
-	    public String manageGroup(Model model) {
-	        Group group = groupService.getGroupDetails();
-	        model.addAttribute("group", group);
-	        return "groupManage";
-	    }
-
-	    @GetMapping("/members")
-	    public String manageMembers(Model model) {
-	        // Load member data
-	        return "memberManage";
-	    }
-
-	    @GetMapping("/schedule")
-	    public String manageSchedule(Model model) {
-	        // Load schedule data
-	        return "scheduleManage";
-	    }
-
-	    @GetMapping("/photos")
-	    public String managePhotos(Model model) {
-	        // Load photo data
-	        return "photoManage";
-	    }
-	    */
+	
 }
